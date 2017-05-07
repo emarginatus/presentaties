cb53d93ce1c85f75f32b1e9c6e62e4daed91b721
ThierryO <Thierry.Onkelinx@inbo.be>
2017-05-07 20:54:15 +0200

    render presentaties via wercker

---
+++ werkgroep/render.R
@@ -0,0 +1,20 @@
+basedir <- getwd()
+if (!file_test("-d", "_site")) {
+  dir.create("_site")
+}
+to_do <- list.files(
+  path = basedir,
+  pattern = "*.Rmd",
+  recursive = TRUE,
+  full.names = TRUE
+)
+for (filename in to_do) {
+  setwd(dirname(filename))
+  rmarkdown::render(basename(filename))
+  file.copy(
+    from = gsub("\\.Rmd", "\\.html", basename(filename)),
+    to = paste0(basedir, "/_site"),
+    overwrite = TRUE
+  )
+}
+setwd(basedir)
